017db3d032e261b7f6d3682524d6d35a82257dc4
Quinn Tsai <quinntsai@altek.com.tw>
2021-09-01 18:25:30 +0800

    Remove unused debug log.

--- vl53l0x_i2c_platform.py
+++ vl53l0x_i2c_platform.py
@@ -1,4 +1,6 @@
 from smbus import SMBus
+from enum import IntEnum
+
 
 STATUS_OK = 0
 STATUS_FAIL = 1
@@ -7,21 +9,22 @@ VL53L0X_REG_RESULT_RANGE_STATUS = 0x0014
 VL53L0X_REG_IDENTIFICATION_REVISION_ID = 0x00c2
 
 
-VL53L0X_DEVICEERROR_NONE = 0
-VL53L0X_DEVICEERROR_VCSELCONTINUITYTESTFAILURE = 1
-VL53L0X_DEVICEERROR_VCSELWATCHDOGTESTFAILURE = 2
-VL53L0X_DEVICEERROR_NOVHVVALUEFOUND = 3
-VL53L0X_DEVICEERROR_MSRCNOTARGET = 4
-VL53L0X_DEVICEERROR_SNRCHECK = 5
-VL53L0X_DEVICEERROR_RANGEPHASECHECK = 6
-VL53L0X_DEVICEERROR_SIGMATHRESHOLDCHECK = 7
-VL53L0X_DEVICEERROR_TCC = 8
-VL53L0X_DEVICEERROR_PHASECONSISTENCY = 9
-VL53L0X_DEVICEERROR_MINCLIP = 10
-VL53L0X_DEVICEERROR_RANGECOMPLETE = 11
-VL53L0X_DEVICEERROR_ALGOUNDERFLOW = 12
-VL53L0X_DEVICEERROR_ALGOOVERFLOW = 13
-VL53L0X_DEVICEERROR_RANGEIGNORETHRESHOLD = 14
+class ErrorStatus(IntEnum):
+    VL53L0X_DEVICEERROR_NONE = 0
+    VL53L0X_DEVICEERROR_VCSELCONTINUITYTESTFAILURE = 1
+    VL53L0X_DEVICEERROR_VCSELWATCHDOGTESTFAILURE = 2
+    VL53L0X_DEVICEERROR_NOVHVVALUEFOUND = 3
+    VL53L0X_DEVICEERROR_MSRCNOTARGET = 4
+    VL53L0X_DEVICEERROR_SNRCHECK = 5
+    VL53L0X_DEVICEERROR_RANGEPHASECHECK = 6
+    VL53L0X_DEVICEERROR_SIGMATHRESHOLDCHECK = 7
+    VL53L0X_DEVICEERROR_TCC = 8
+    VL53L0X_DEVICEERROR_PHASECONSISTENCY = 9
+    VL53L0X_DEVICEERROR_MINCLIP = 10
+    VL53L0X_DEVICEERROR_RANGECOMPLETE = 11
+    VL53L0X_DEVICEERROR_ALGOUNDERFLOW = 12
+    VL53L0X_DEVICEERROR_ALGOOVERFLOW = 13
+    VL53L0X_DEVICEERROR_RANGEIGNORETHRESHOLD = 14
 
 
 
@@ -46,10 +49,9 @@ def VL53L0X_GetProductRevision(version:dict):
     VL53L0X_RdByte(VL53L0X_REG_IDENTIFICATION_REVISION_ID, data_list)
 
     # print("len of data_list = {}".format(len(data_list)))
-    # if len(data_list) > 0:
-    #     print("{}  {}".format(type(data_list[0]), data_list[0]))
 
     if len(data_list) > 0:
+    #     print("{}  {}".format(type(data_list[0]), data_list[0]))
         version["major"] = 1
         version["minor"] = (data_list[0] & 0xf0) >> 4
     else:
@@ -64,11 +66,10 @@ def VL53L0X_GetDeviceErrorStatus(dev_data:dict):
     data_list = []
     VL53L0X_RdByte(VL53L0X_REG_RESULT_RANGE_STATUS, data_list)
 
-    print("len of data_list = {}".format(len(data_list)))
-    if len(data_list) > 0:
-        print("{}  {}".format(type(data_list[0]), data_list[0]))
+    # print("len of data_list = {}".format(len(data_list)))
 
     if len(data_list) > 0:
+        # print("{}  {}".format(type(data_list[0]), data_list[0]))
         dev_data["err status"] = (data_list[0] & 0x78) >> 3
     else:
         status = STATUS_FAIL
@@ -82,10 +83,10 @@ def VL53L0X_GetPowerMode(dev_data:dict):
     data_list = []
     VL53L0X_RdByte(0x80, data_list)
 
-    print("len of data_list = {}".format(len(data_list)))
+    # print("len of data_list = {}".format(len(data_list)))
 
     if len(data_list) > 0:
-        print("{}  {}".format(type(data_list[0]), data_list[0]))
+        # print("{}  {}".format(type(data_list[0]), data_list[0]))
         if data_list[0] == 1:
             dev_data["power mode"] = "IDLE"
         else:
